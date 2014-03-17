256589154459e4d08159c0c0740d3e71740ed70a
creeves <cam@quaypay.com>
2014-03-18 10:44:06 +1100

    Styles

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,12 +4,17 @@ angular.module('confApp')
   .controller('MainCtrl', ['$scope', '$location', function ($scope, $location) {
     
   	$scope.name = function(e) {
-  		var username;
-  		if (e === undefined || e == "") {
-  			username = 'user' + parseInt(Math.random()*1000000);
-  		} else {
-  			username = e;
-  		}
-  		$location.path('/conf').search('user', username);
-  	}
+      var username;
+      if (e === undefined || e == "") {
+        username = 'user' + parseInt(Math.random()*1000000);
+      } else {
+        username = e;
+      }
+      $location.path('/conf').search('user', username);
+    }
+    $scope.join = function(e) {
+      if (e.keyCode == 13) {
+        $scope.name($scope.confname);
+      }
+    }
   }]);
